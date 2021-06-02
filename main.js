b39cfbeed1e8f22fb6b807776170c390bf65fe63
Naintara7470 <82662912+Naintara7470@users.noreply.github.com>
2021-06-02 05:12:48 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
- var song1 = "";
- var song2 = "";
+ song1 = "";
+ song2 = "";
+
+ song1_status = "";
+ song2_status = "";
+
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
+ rightWristX = "";
+ rightWristY = "";
+
+ leftWristX = "";
+ leftWristY = "";
 function preload(){
     song1 = loadSound("Count on me.mp3");
     song2 = loadSound("Happier.mp3");
@@ -9,13 +21,72 @@ function setup(){
     canvas.center();
 video = createCapture(VIDEO);
 video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+console.log('Posenet is intialized');
+}
+function gotPoses(results){
+    if(results.length > 0)
+  {
+	console.log(results);
+	scoreRightWrist =  results[0].pose.keypoints[10].score;
+	scoreLeftWrist =  results[0].pose.keypoints[9].score;
+	console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
+	
+	rightWristX = results[0].pose.rightWrist.x;
+	rightWristY = results[0].pose.rightWrist.y;
+	console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+
+	leftWristX = results[0].pose.leftWrist.x;
+	leftWristY = results[0].pose.leftWrist.y;
+	console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+		
+  }
 }
 function draw(){
     image(video,0,0,600,500);
-}
-function play(){
+
+    song1_status = song1.isPlaying();
+	song2_status = song2.isPlaying();
+    fill("#000066");
+    stroke("#000066");
+    if(scoreRightWrist > 0.2)
+	{ 
+		circle(rightWristX,rightWristY,20);
+
+			song2.stop();
+
+		if(song1_status == false)
+		{
+			song1.play();
+			document.getElementById("song").innerHTML = "Playing - Count on Me song "
+		}
+	}
+
+	if(scoreLeftWrist > 0.2)
+	{
+		circle(leftWristX,leftWristY,20);
+
+			song1.stop();
+
+		if(song2_status == false)
+		{
+			song2.play();
+			document.getElementById("song").innerHTML = "Playing - Happier song"
+		}
+	}
 
 }
+
+
+function play(){
+song.play();
+song.setVolume(1);
+song.rate(1);
+}
 function loadSound(){
-    
+
 }
